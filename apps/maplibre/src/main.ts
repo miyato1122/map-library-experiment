611d619e4f9a4836e662fccc33d18ae704cb27f6
Tomoaki Higa <tomoaki.higa@create-sekai.com>
2026-03-02 13:17:00 +0900

    feat: NuxtUIをインストール

--- apps/maplibre/src/main.ts
+++ apps/maplibre/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
+import { createRouter, createMemoryHistory } from "vue-router";
+import ui from "@nuxt/ui/vue-plugin";
 import App from "./App.vue";
 import "./style.css";
 
-createApp(App).mount("#app");
+const router = createRouter({ history: createMemoryHistory(), routes: [] });
+
+createApp(App).use(router).use(ui).mount("#app");
